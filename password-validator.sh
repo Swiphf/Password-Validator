0da328eb3703ed5b44631b4527232eda70e5cc53
Swiphf <90255849+Swiphf@users.noreply.github.com>
2021-11-06 15:54:33 +0200

    Update password-validator.sh

--- password-validator.sh
+++ password-validator.sh
@@ -1,76 +1,39 @@
-#! /bin/bash
+#!/bin/bash
 
-RED='\033[0;31m'   # red
-GREEN='\033[0;32m' # green
+input=$1
+error_counter=0
 
-function validate() {
+green='\033[1;92m' # Valid
+red='\033[1;91m'   # Invalid
 
-    text=$1    
+# Check whether the input text is longer than 10 digits or not
+if ! [[ ${#input} -ge 10 ]];
+then 
+    echo -e "${red}Password must contain at least 10 chars" && error_counter=1
+fi
 
-    if checkLength $text; then
-        if containsNumbers $text; then
-            if containsLowercase $text; then
-                if containsUppercase $text; then
-                    if containSpecialChars $text; then
-                        printf "${GREEN}Valid\\n"
-                        return 0
-                    else
-                        printf "${RED}Password must contain special characters\\n"
-                        return 1
-                    fi
-                else
-                    printf "${RED}Password must contain upper case letters\\n"
-                    return 1
-                fi
-            else
-                printf "${RED}Password must contain lower case letters\\n"
-                return 1
-            fi
-        else
-            printf "${RED}Password must contain numbers\\n"
-            return 1
-        fi
-    else
-        printf "${RED}Password must contain at least 10 chars\\n"
-        return 1
-    fi
-}
-function checkLength() {
-    str=$1
-    length=${#str}
-    if [ $length -ge 10 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-function containsNumbers() {
-    if [[ $1 =~ [0-9] ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-function containsLowercase() {
-    if [[ $1 =~ [a-z] ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-function containsUppercase() {
-    if [[ $1 =~ [A-Z] ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-function containSpecialChars() {
-    if [[ $str == *['!'@\#\$%^\&*()_+]* ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
+# Check whether the input text contains capitals
+if ! [[ $input =~ [[:upper:]] ]];
+then 
+    echo -e "${red}Password must contain upper case letters" && error_counter=1 
+fi
 
-validate $1
+# Check whether the input text contains lowercase letters
+if ! [[ $input =~ [[:lower:]] ]];
+then  
+    echo -e "${red}Password must contain lower case letters" && error_counter=1 
+fi
+
+# Check whether the input text contains numbers
+if ! [[ $input =~ [[:digit:]] ]];
+then 
+    echo -e "${red}Password must contain numbers" && error_counter=1
+fi
+
+# Check error_counter. 0 means valid
+if [[ $error_counter -eq 1 ]];
+then
+    echo -e "${red}Password is invalid" && exit 1
+else
+    echo -e "${green}Password is valid" && exit 0
+fi
